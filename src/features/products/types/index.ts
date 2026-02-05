48aa1543a8babc3ab4b98872eb7bfcf8e293fcea
intankirana19 <intankirana19@gmail.com>
2026-02-05 15:52:30 +0700

    create product type

---
+++ src/features/products/types/index.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+
+export const productSchema = z
+  .object({
+    id: z.string(),
+    name: z.string().optional(),
+    price: z.number().optional(),
+    createdAt: z.string().optional(),
+    updatedAt: z.string().optional(),
+  })
+  .passthrough();
+
+export type Product = z.infer<typeof productSchema>;
+
+export const productListSchema = z.array(productSchema);
