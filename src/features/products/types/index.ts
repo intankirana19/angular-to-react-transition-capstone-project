e2e86c266b484c4e63c0f8bbe32cdea09423ef91
intankirana19 <intankirana19@gmail.com>
2026-02-06 14:42:52 +0700

    adjust product type & schema

--- src/features/products/types/index.ts
+++ src/features/products/types/index.ts
@@ -1,15 +1,16 @@
+// pakai zod bisa infer tipe TypeScript sekaligus validasi pas runtime + parsing/coercion
 import { z } from 'zod';
 
 export const productSchema = z
   .object({
     id: z.string(),
     name: z.string().optional(),
-    price: z.number().optional(),
+    price: z.coerce.number().optional(), // data price dari mock api type string jd coerce ke number biar di seluruh app bisa hitung, sort, dan format harga tanpa konversi berulang?
     createdAt: z.string().optional(),
     updatedAt: z.string().optional(),
   })
-  .passthrough();
+  // .passthrough(); // blm perlu? unknown keys behavior/ kalo ada field data baru: defaultnya strip -> (dibuang), strict() -> error, passthrough() -> field baru tetap disimpan.
+  
 
-export type Product = z.infer<typeof productSchema>;
-
-export const productListSchema = z.array(productSchema);
+export type Product = z.infer<typeof productSchema>; // tipe produk untuk dipakai di seluruh app.
+export const productListSchema = z.array(productSchema); // skema list produk (array dari product).
