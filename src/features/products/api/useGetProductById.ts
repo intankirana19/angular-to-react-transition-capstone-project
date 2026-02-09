02bae0c0da6e10968dff2df8d690032e8d7763ce
intankirana19 <intankirana19@gmail.com>
2026-02-09 14:35:34 +0700

    create useGetProductById hook

---
+++ src/features/products/api/useGetProductById.ts
@@ -0,0 +1,16 @@
+import { useQuery } from '@tanstack/react-query';
+import { type Product } from '../types';
+import { getProductById } from '../services/productsService';
+
+export function useGetProductById(productId?: string) {
+  return useQuery<Product, Error>({
+    queryKey: ['products', productId],
+    queryFn: () => {
+      if (!productId) {
+        throw new Error('Product ID is required');
+      }
+      return getProductById(productId);
+    },
+    enabled: Boolean(productId),
+  });
+}
